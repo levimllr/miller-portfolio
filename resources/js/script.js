9890d3a5a0d56097dc749d6af68e82cef0efdaea
Levi Miller <paullevimiller@gmail.com>
2019-03-25 17:51:21 -0700

    Two project pages added.

--- resources/js/script.js
+++ resources/js/script.js
@@ -26,3 +26,70 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+var modal;
+var img;
+var modalImg;
+var captionText;
+
+function triggerModal(clicked_id) {
+  var id = clicked_id;
+  console.log(id);
+  // Get the modal
+  modal = document.getElementById(id + '-modal');
+
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+
+  img = document.getElementById(id);
+  modalImg = document.getElementById(id + "-modalImg");
+  captionText = document.getElementById(id + "-caption");
+
+  modal.style.display = "block";
+  modalImg.src = img.src;
+  captionText.innerHTML = "<p style='text-align: center; color: #fff'>" + img.alt + "</p>";
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+
+
+
+
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   captionText.innerHTML = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
