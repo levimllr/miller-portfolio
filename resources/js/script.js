a8f361f46d963d264abaf7c79ade763b1f7d2800
Levi Miller <paullevimiller@gmail.com>
2019-03-26 16:45:25 -0700

    Buttons that scroll to section!

--- resources/js/script.js
+++ resources/js/script.js
@@ -70,13 +70,71 @@ $(document).ready(function() {
   }, {
       offset: '60px;'
   });
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+  }
+
+  $('.prev').click(function () {
+    plusSlides(-1)
+  });
+  $('.next').click(function () {
+    plusSlides(1)
+  });
+  $('.dot1').click(function () {
+    currentSlide(1)
+  });
+  $('.dot2').click(function () {
+    currentSlide(2)
+  });
+  $('.dot3').click(function () {
+    currentSlide(3)
+  });
+
   
   /* Scroll on buttons */
-  $('.js--scroll-to-plans').click(function () {
-      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  $('.js--scroll-to-main').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-main').offset().top}, 1000);
   });
-  $('.js--scroll-to-start').click(function () {
-      $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+  $('.js--scroll-to-contact').click(function () {
+      $('html, body').animate({scrollTop: $('.js--section-contact').offset().top}, 1000);
+  });
+  $('.js--scroll-to-gr').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-project-gr').offset().top}, 1000);
+  });
+  $('.js--scroll-to-dmb').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-project-dmb').offset().top}, 1000);
+  });
+  $('.js--scroll-to-ph').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-project-ph').offset().top}, 1000);
+  });
+  $('.js--scroll-to-top').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-main').offset().top}, 1000);
   });
   
   // Select all links with hashes
@@ -171,33 +229,34 @@ $(document).ready(function() {
 
 });
 
+
 // var slideIndex = 1;
 // showSlides(slideIndex);
 
 // // Next/previous controls
 // function plusSlides(n) {
-//   showSlides(slideIndex += n);
+// showSlides(slideIndex += n);
 // }
 
 // // Thumbnail image controls
 // function currentSlide(n) {
-//   showSlides(slideIndex = n);
+// showSlides(slideIndex = n);
 // }
 
 // function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1} 
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none"; 
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block"; 
-//   dots[slideIndex-1].className += " active";
+// var i;
+// var slides = document.getElementsByClassName("mySlides");
+// var dots = document.getElementsByClassName("dot");
+// if (n > slides.length) {slideIndex = 1} 
+// if (n < 1) {slideIndex = slides.length}
+// for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none"; 
+// }
+// for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+// }
+// slides[slideIndex-1].style.display = "block"; 
+// dots[slideIndex-1].className += " active";
 // }
 
 // var modal;
